6020b59739abe2ff021cb83d9ad7a202ad6e3894
mDjeee <isejtniyazov@mail.ru>
2022-01-26 19:48:06 +0500

    feat: solve the problem

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = '';
+    let sub_string = '';
+    let arr;
+    let code_num;
+    let code_morse;
+    for(let i=0; i<expr.length; i+=10){
+        sub_string = expr.substring(i, i+10);
+        if( sub_string === '**********' ){
+            str += ' ';
+        }
+        else {
+            arr = sub_string.split('');
+            code_morse = '';
+            for(let j=0; j<arr.length; j+=2){
+                if(arr[j]!== '0'){
+                    if(arr[j+1] === '0'){
+                        code_morse += '.';
+                    }
+                    else {
+                        code_morse += '-';
+                    }
+                }
+
+            }
+            str += MORSE_TABLE[code_morse];
+        }
+    }
+    return str;
 }
 
 module.exports = {
